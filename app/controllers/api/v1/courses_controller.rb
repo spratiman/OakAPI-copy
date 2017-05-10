045ed0790b2818f2a6b809411430cbcb315dc977
Chintan Shah <chintans98@gmail.com>
2017-05-09 23:47:46 -0400

    Allow for GET requests on courses/comments/ratings without auth
    - this fixes issue #5, you can read more on the bug there
    - in general, we are just allowing access to the API without authentication when the user sends a GET request for course info with its ratings and comments
    
    Signed-off-by: Chintan Shah <chintans98@gmail.com>

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CoursesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:index, :show]
 
   # GET /courses
   def index
