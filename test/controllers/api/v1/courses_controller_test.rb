539936cd8e745148344813ec52cf9a278ba93b8c
Chintan Shah <chintans98@gmail.com>
2017-05-10 00:53:47 -0400

    Add tests that effectively make sure issue #5 is fixed
    - tests are for making sure we can access the GET routes for courses/comments/ratings with and without authenticated correctly
    
    Signed-off-by: Chintan Shah <chintans98@gmail.com>

--- test/controllers/api/v1/courses_controller_test.rb
+++ test/controllers/api/v1/courses_controller_test.rb
@@ -14,13 +14,38 @@ class Api::V1::CoursesControllerTest < ActionDispatch::IntegrationTest
     @course = nil
   end
 
+  '''
+  Testing for the ability to get all the courses with and without authentication
+  '''
+  test "should get index without auth" do
+    get courses_url, headers: {'Accept' => 'application/vnd.oak.v1'}, as: :json
+    assert_response :success
+  end
+
   test "should get index" do
     get courses_url, headers: @auth_headers, as: :json
     assert_response :success
   end
 
+  '''
+  Testing for the ability to look at a single course with and without user authentication
+  '''
+  test "should get show without auth" do
+    get course_url(@course), headers: {'Accept' => 'application/vnd.oak.v1'}, as: :json
+    assert_response :success
+  end
+
   test "should get show" do
     get course_url(@course), headers: @auth_headers, as: :json
+    assert_response :success
+  end
+
+  '''
+  Testing for the ability to look at a single course and make sure its the correct one
+  with and without authentication
+  '''
+  test "show should show course without auth" do
+    get course_url(@course), headers: {'Accept' => 'application/vnd.oak.v1'}, as: :json
     assert_response :success
   end
 
