7aae5eb764f506af5c88b6ec6b978797d74a1ad3
Angeline <angelineyasodhara@gmail.com>
2017-05-18 23:05:01 -0400

    Fixed bugs
    Previously, the progress for downloaded goes above 100% because the
    content_length_proc doesn’t return the full download size. The script
    is updated to reflect the changes accordingly.
    
    Also, added a check to see if there’s any migration pending.

--- lib/assets/update_course.rb
+++ lib/assets/update_course.rb
@@ -10,6 +10,10 @@ dbconf = YAML.load_file(File.join(__dir__, "/../../config", "database.yml"))
 ActiveRecord::Base.establish_connection(
   dbconf["development"]
 )
+
+# Check for pending migration
+ActiveRecord::Migration.check_pending!
+
 # Retrieve courses directly from dataset available by the name courses.json
 # File can be obtained on https://github.com/cobalt-uoft/datasets
 puts "Downloading courses data..."
@@ -17,23 +21,23 @@ total_size = 0
 page_content = open("https://github.com/cobalt-uoft/datasets/raw/master/courses.json",
   :content_length_proc => lambda {|t|
     if t && 0 < t
-      puts "Total size to be downloaded: " + t.to_s
-      total_size = t
+      puts "Bytes downloaded:"
     end
   },
   :progress_proc => lambda {|p|
-    print "%.1f%% downloaded" % (p/(total_size*1.0) * 100)
+    print p
     print "\r"
+    total_size = p
   })
 
-
+puts total_size
 puts "Data downloaded, start processing..."
 progress = 0
 page_content.each_line { |line|
   course = JSON.parse(line)
   Course.update_db(course)
   progress += line.size
-  print "%.1f%% done" % (progress/(total_size * 1.0) * 10)
+  print "%.1f%% done" % (progress/(total_size * 1.0) * 100)
   print "\r"
 }
 puts "Done processing"
