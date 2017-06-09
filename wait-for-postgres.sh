33358e3d1004a83b0d8320fefc8f7e56d674c1f6
umar-ahmed <umar.ahmed1998@gmail.com>
2017-06-09 17:53:11 -0400

    Fix line endings

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -6,10 +6,10 @@ host="$1"
 shift
 cmd="$@"
 
-until psql -h "$host" -U "postgres" -c '\l'; do
-  >&2 echo "Postgres is unavailable - sleeping"
+>&2 echo -e "\e[93mPostgres is unavailable - waiting\e[39m"
+until psql -h "$host" -U "postgres" -c '\q' &>/dev/null; do
+  sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
-bundle exec rails db:setup
+>&2 echo -e "\e[32mPostgres is up - executing command\e[39m"
 exec $cmd
